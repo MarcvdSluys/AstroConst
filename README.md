3ec59cc7a114a23f722166dd769e8546314af498
MvdS <git@vandersluys.nl>
2022-01-04 22:05:27 +0100

    Fix README.md and __init__.py.

--- README.md
+++ README.md
@@ -5,11 +5,12 @@ Downloads](https://img.shields.io/pypi/dm/astroconst) [![Documentation
 Status](https://readthedocs.org/projects/astroconst/badge/?version=latest)](https://astroconst.readthedocs.io/en/latest/?badge=latest)
 ![PyPI - License](https://img.shields.io/pypi/l/astroconst?color=%230A0)
 
-A Python package that provides astronomical constants. The code is being developed by [Marc van der Sluys](http://marc.vandersluys.nl) of the
-department of Astrophysics at the Radboud University Nijmegen, the Institute of Nuclear and High-Energy
-Physics (Nikhef), and the Institute for Gravitational and Subatomic Physics (GRASP) at Utrecht University, all
-in The Netherlands.  The AstroConst package can be used under the conditions of the [EUPL
-1.2](https://www.eupl.eu/1.2/en/) licence.
+A Python package that provides astronomical constants. 
+The code is being developed by [Marc van der Sluys](http://marc.vandersluys.nl) of the department of 
+Astrophysics at the Radboud University Nijmegen, the Institute of Nuclear and High-Energy Physics (Nikhef), 
+and the Institute for Gravitational and Subatomic Physics (GRASP) at Utrecht University, all in The 
+Netherlands.  The AstroConst package can be used under the conditions of the 
+[EUPL 1.2](https://www.eupl.eu/1.2/en/) licence.
 
 
 ## Installation ##
@@ -44,3 +45,7 @@ import astroconst
 
 ## References ##
 
+* [The Astronomical Almanac Online!](http://asa.hmnao.com/SecK/Constants.html)
+
+
+<sub>Copyright (c) 2022 Marc van der Sluys</sub>
--- astroconst/__init__.py
+++ astroconst/__init__.py
@@ -1,27 +1,25 @@
 # -*- coding: utf-8 -*-
 #  Copyright (c) 2022  Marc van der Sluys - marc.vandersluys.nl
 #   
-#  This file is part of the AstroConst Python package, containing a Python module to obtain and read Dutch
-#  weather data from AstroConst.nl.  See: https://github.com/MarcvdSluys/AstroConst
+#  This file is part of the AstroConst Python package:
+#  A Python package that provides astronomical constants.
+#  See: https://github.com/MarcvdSluys/AstroConst
 #   
-#  This is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-#  
-#  This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
-#  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-#  
-#  You should have received a copy of the GNU General Public License along with this code.  If not, see
-#  <http://www.gnu.org/licenses/>.
+#  This is free software: you can redistribute it and/or modify it under the terms of the European Union
+#  Public Licence 1.2 (EUPL 1.2).  This software is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#  PURPOSE.  See the EU Public License for more details.  You should have received a copy of the European
+#  Union Public License along with this code.  If not, see <https://www.eupl.eu/1.2/en/>.
 
 
 """AstroConst package
 
-A Python package that provides astronomical constants.  The code is being developed by
-`Marc van der Sluys <http://marc.vandersluys.nl>`_ of the department of Astrophysics at the Radboud
-University Nijmegen, the Institute of Nuclear and High-Energy Physics (Nikhef), and the Institute for
-Gravitational and Subatomic Physics (GRASP) at Utrecht University, all in The Netherlands.  The AstroConst
-package can be used under the conditions of the EUPL 1.2 licence.  These pages contain the API documentation.
-For more information on the Python package, licence and source code, see the
+A Python package that provides astronomical constants.
+The code is being developed by `Marc van der Sluys <http://marc.vandersluys.nl>`_ of the department of
+Astrophysics at the Radboud University Nijmegen, the Institute of Nuclear and High-Energy Physics (Nikhef),
+and the Institute for Gravitational and Subatomic Physics (GRASP) at Utrecht University, all in The Netherlands.
+The AstroConst package can be used under the conditions of the EUPL 1.2 licence.  These pages contain the API
+documentation.  For more information on the Python package, licence and source code, see the
 `AstroConst GitHub page <https://github.com/MarcvdSluys/AstroConst>`_.
 """
 
